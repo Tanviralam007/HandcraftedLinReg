37d338c1ffad243cb005681613864cc8eede1248
Tanviralam007 <tanviralam451@gmail.com>
2025-02-15 20:52:04 +0600

    implementde LinearRegression class interface with fit and predict methods

---
+++ include/LinearRegression.h
@@ -0,0 +1,29 @@
+#ifndef LINEAR_REGRESSION_H
+#define LINEAR_REGRESSION_H
+
+#include <vector>
+class LinearRegression{
+    private:
+        double m;
+        double b;
+        double learning_rate;
+        int num_iterations;
+    
+    public:
+        LinearRegression(double learning_rate, int num_iterations);
+
+        // method to train(fit) the model using the training data
+        void fit(const std::vector<std::pair<double, double>>& training_data);
+
+        // method to make predictions using the trained model
+        double predict(double experience);
+
+        // method to calculate the mean squared error (MSE) of the model
+        double mean_squared_error(const std::vector<std::pair<double, double>>& data);
+
+        // getter methods for the model parameters (m and b)
+        double get_slope();
+        double get_intercept();
+};
+
+#endif // LINEAR_REGRESSION_H
