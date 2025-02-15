b694bb4b00ff1ac65a196de0a0fb7353d0a97a8e
Tanviralam007 <tanviralam451@gmail.com>
2025-02-15 20:56:49 +0600

    Implemented LinearRegression class methods

---
+++ src/LinearRegression.cpp
@@ -0,0 +1,7 @@
+#include "../include/LinearRegression.h"
+
+LinearRegression::LinearRegression(double learning_rate, int num_iterations) {
+    this->learning_rate = learning_rate;
+    this->num_iterations = num_iterations;
+}
+
